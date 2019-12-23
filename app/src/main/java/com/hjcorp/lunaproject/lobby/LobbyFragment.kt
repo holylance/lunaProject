7d46f6d99b24a139a4a39b98822e8574c87657d3
holylance <holylance82@gmail.com>
2019-12-23 20:42:18 +0100

    feature: Add navigation path and buttons for games

--- app/src/main/java/com/hjcorp/lunaproject/lobby/LobbyFragment.kt
+++ app/src/main/java/com/hjcorp/lunaproject/lobby/LobbyFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.hjcorp.lunaproject.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.lobby_fragment.*
 
 class LobbyFragment : Fragment() {
 
@@ -21,6 +24,16 @@ class LobbyFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.lobby_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button_game1.setOnClickListener {
+            nav_host.findNavController().navigate(R.id.action_lobbyFragment_to_game1Fragment)
+        }
+        button_game2.setOnClickListener {
+            nav_host.findNavController().navigate(R.id.action_lobbyFragment_to_game2Fragment)
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
